994297521f2f42ade5b01f7fccf11581b94ec48e
bertdadzie <dadzie2017j@gmail.com>
2021-02-20 05:13:36 -0800

    first commit

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-property-card',
+  //template:``
+  templateUrl:'property-card.component.html',
+  styleUrls:['property-card.component.css']
+}
+
+)
+
+
+
+export class PropertyCardComponent {
+
+}
